e047592ba29bd23b9dad96f22b84096386bde071
Tiaan Kleyn <Tiaankleyn20@gmail.com>
2026-02-26 16:49:07 +0200

    Update App.py

--- Main/App.py
+++ Main/App.py
@@ -7,7 +7,6 @@ ROOT = Path(__file__).parent
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
-from ui.dashboard import render
 
 st.set_page_config(page_title="LTL Pathfinder (Basic)", layout="wide")
 
